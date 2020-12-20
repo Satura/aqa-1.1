05de1deab322ef9234e6249f5ace42e4009b5eae
Anastasia Sv <selen500@mail.ru>
2020-12-20 20:29:58 +0500

    useJUnitPlatform (2)

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
+    // JUnit4
+
     @Test
     public void shouldRemain900() {
         CashbackHackService service = new CashbackHackService();
@@ -23,4 +25,25 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         assertEquals(0, service.remain(1000));
     }
+
+    //JUnit5
+
+    @org.junit.jupiter.api.Test
+    public void shouldRemain900j5() {
+        CashbackHackService service = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(900, service.remain(100));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldRemain300j5() {
+        CashbackHackService service = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(300, service.remain(700));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldRemain0j5() {
+        CashbackHackService service = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
+    }
+
 }
